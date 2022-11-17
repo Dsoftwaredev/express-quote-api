d220d380367e44bdc2a5d6e3d4b7a19f92083434
Dsoftwaredev <danielsoftwaredev@outlook.com>
2022-11-17 22:31:06 +0000

    get random quote working

--- quotes.js
+++ quotes.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 const quotesRouter = express.Router();
 const { quotes } = require('./data');
@@ -6,9 +7,19 @@ const { getRandomElement } = require('./utils');
 
 
 
+// get all quotes
 quotesRouter.get('/',(req, res, next) => {
     res.send(quotes)
     
+})
+
+quotesRouter.get('/random',(req, res, next) => {
+    const randQ = getRandomElement(quotes);
+    if (randQ) {
+        res.send(randQ);
+    } else {
+        res.status(404).send();
+    }
 })
 
 module.exports = quotesRouter;
